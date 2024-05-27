10b6dfb7664350da3e356bc0d8e33c41f9fcde73
Car-V <vale.c@northeastern.edu>
2024-05-27 17:55:12 -0400

    Installed Unity UI

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 //using Unity.VisualScripting;
 using UnityEngine;
 
+// need to make enemy die too
+
 public class EnemyBehavior : MonoBehaviour
 {
     public Transform playerMoveToward;
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public AudioClip deadSFX;
+    public Slider healthSlider;
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = startingHealth;
+        healthSlider.value = currentHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth -= damageAmount;
+            healthSlider.value = currentHealth;
+        }
+        if (currentHealth <= 0)
+        {
+            PlayerDies();
+        }
+
+        Debug.Log("Current health: " + currentHealth);
+    }
+
+    void PlayerDies()
+    {
+        Debug.Log("Player is dead...");
+        AudioSource.PlayClipAtPoint(deadSFX, transform.position);
+        transform.Rotate(-90, 0, 0, Space.Self); // Space.Self, according to local rotation
+    }
+
+    public void TakeHealth(int healthAmount)
+    {
+        if (currentHealth < 100)
+        {
+            currentHealth += healthAmount;
+            healthSlider.value = Mathf.Clamp(currentHealth, 0, 100); // so we don't overflow
+        }
+
+        Debug.Log("Current health with loot: " + currentHealth);
+    }
+}
